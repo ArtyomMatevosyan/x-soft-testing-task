63183c484f9085ff2746e9365f7ec83209959ae0
ArtyomMatevosyan <matevosyanartyom98@gmail.com>
2022-01-03 17:57:47 +0400

    creating task

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,11 @@
+import Header from '../header/Header'
+
+const Home = () => {
+    return(
+        <>
+        <Header></Header>
+        </>
+    )
+}
+
+export default Home
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,12 @@
+
+const Header = () => {
+    return(
+        <div className='header'>
+            <div className='header_container'>
+
+            </div>
+        </div>
+    )
+}
+
+export default Header
